1a5f1a2610fac191193eaaee4c53161061908b47
navnit0707 <knavnit81@gmail.com>
2023-07-11 23:20:02 +0530

    router setup

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Homepage from "./pages/Homepage";
+import Product from "./pages/Product";
+import Pricing from "./pages/Pricing";
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="product" element={<Product />} />
+        <Route path="pricing" element={<Pricing />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
